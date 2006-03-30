aff512ae51df5cb7becf52dff3c084c61b62cd53
madedit <madedit@f6f7653f-1aae-674b-80c7-1a03238de978>
2006-03-30 10:50:32 +0000

    fixed wxListEvent compiling error

--- madedit/src/MadHighlightingDialog.cpp
+++ madedit/src/MadHighlightingDialog.cpp
@@ -434,7 +434,7 @@ void MadHighlightingDialog::WxListBoxSyntaxSelected(wxCommandEvent& event)
     WxListCtrlKeyword->Thaw();
 
     g_Index=-1;
-    wxListEvent event;
+    wxListEvent event(0);
     event.m_itemIndex=0;
     WxListCtrlKeywordSelected(event);
 }
