2f53200c746c6a68e3c5753ae7598dc3427ebde6
madedit <madedit@f6f7653f-1aae-674b-80c7-1a03238de978>
2006-08-16 06:21:50 +0000

    vs.net2003 -> vc71

--- madedit/chmod_644_all_files.sh
+++ madedit/chmod_644_all_files.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-find devcpp images image2xpm libunicows po src syntax vs.net2003 xpressive wxFlatNotebook -type f -exec chmod 644 {} \;
+find devcpp images image2xpm libunicows po src syntax vc71 xpressive wxFlatNotebook -type f -exec chmod 644 {} \;
