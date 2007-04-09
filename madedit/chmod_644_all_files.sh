f59231f5d918f10ad80f1cbdc2897837d4bbcd2f
madedit <madedit@f6f7653f-1aae-674b-80c7-1a03238de978>
2007-04-09 16:37:52 +0000

    updated config files for Linux

--- madedit/chmod_644_all_files.sh
+++ madedit/chmod_644_all_files.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-find devcpp images image2xpm libunicows po src syntax vc71 xpressive wxFlatNotebook -type f -exec chmod 644 {} \;
+find devcpp images image2xpm libunicows po src syntax vc71 xpressive wxAUI -type f -exec chmod 644 {} \;
