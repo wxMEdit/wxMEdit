3959e77b2643846246ae09aba6d6a43207d36f58
hltj <jiaywe@gmail.com>
2013-06-02 12:00:46 +0800

    #12 bakefile: add msvc nmake support

---
+++ build/vcmak.bat
@@ -0,0 +1,7 @@
+rem set VC_BOOST_DIR=
+rem set VC_ICU_DIR=
+rem set VC_WXWIN=
+rem set VC_XPRESSIVE_INC=
+rem set VC_AUI_INC=
+rem set VC_AUI_LIB=
+bakefile -D BOOST_DIR="%VC_BOOST_DIR%" -D ICU_DIR="%VC_ICU_DIR%" -D WXWIN="%VC_WXWIN%" -D XPRESSIVE_INC="%VC_XPRESSIVE_INC%" -D AUI_INC="%VC_AUI_INC%" -D AUI_LIB="%VC_AUI_LIB%" -f msvc wxmedit.bkl -o ..\msvc\Makefile
