739ef383b1da8953661a8dfa2b2b50271394dc5e
hltj <jiaywe@gmail.com>
2013-07-26 18:11:49 +0800

    add test case of single-byte encodings conversion

---
+++ test/test.cpp
@@ -0,0 +1,7 @@
+#include "test.h"
+
+int test_main(int, char** const)
+{
+	test_singlebyte_conv();
+	return 0;
+}
---
+++ test/test.h
@@ -0,0 +1,6 @@
+#ifndef WXMEDIT_TEST_H
+#define WXMEDIT_TEST_H
+
+void test_singlebyte_conv();
+
+#endif
