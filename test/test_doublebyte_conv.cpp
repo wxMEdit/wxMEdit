0668ccdc9660d5077e9a63d9463fc39d44bcfeb5
hltj <jiaywe@gmail.com>
2013-08-04 00:51:41 +0800

    enable building wxmedit_test and testing refactored Double-byte conversion on UNIX

--- test/test_doublebyte_conv.cpp
+++ test/test_doublebyte_conv.cpp
@@ -22,8 +22,10 @@ typedef std::map<std::string, U2MBDataMap> U2MBMap;
 U2MBMap u2mb;
 std::vector<std::string> test_doublebyte_conv_enc_list;
 
+#ifdef __WXMSW__
 void mb2u_cp932_init();
 void u2mb_cp932_init();
+#endif
 
 void mb2u_cp936_init();
 void u2mb_cp936_init();
@@ -34,13 +36,17 @@ void u2mb_cp949_init();
 void mb2u_cp950_init();
 void u2mb_cp950_init();
 
+#ifdef __WXMSW__
 void mb2u_euc_jp_init();
 void u2mb_euc_jp_init();
+#endif
 
 void test_doublebyte_conv_init()
 {
+#ifdef __WXMSW__
 	mb2u_cp932_init();
 	u2mb_cp932_init();
+#endif
 
 	mb2u_cp936_init();
 	u2mb_cp936_init();
@@ -51,7 +57,9 @@ void test_doublebyte_conv_init()
 	mb2u_cp950_init();
 	u2mb_cp950_init();
 
+#ifdef __WXMSW__
 	mb2u_euc_jp_init();
 	u2mb_euc_jp_init();
+#endif
 
 }
