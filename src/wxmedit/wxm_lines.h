e440c7bdb8f7bee89b1d2898bd03a8bb570fcebc
hltj <jiaywe@gmail.com>
2014-10-16 12:01:16 +0800

    #80 fix crash when saving a empty file

--- src/wxmedit/wxm_lines.h
+++ src/wxmedit/wxm_lines.h
@@ -284,14 +284,18 @@ struct MadLine
     void Get(wxFileOffset pos, wxByte *buf, size_t size)    // get n bytes
     {
         wxASSERT(pos >= 0 && pos + size <= m_Size);
+        if (size == 0)
+            return;
 
         MadBlockIterator biter = m_Blocks.begin();
         if(pos >= biter->m_Size)
+        {
             do
             {
                 pos -= biter->m_Size;
             }
             while(pos >= (++biter)->m_Size);
+        }
 
         size_t cnt;
         while(true)
