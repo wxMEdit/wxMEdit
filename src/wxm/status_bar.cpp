e20a349807641e268c9cbadbe2f6408a76e72d35
hltj <jiaywe@gmail.com>
2023-06-24 10:43:46 +0800

    fix compatibility of status bar field widths

--- src/wxm/status_bar.cpp
+++ src/wxm/status_bar.cpp
@@ -21,26 +21,41 @@ void WXMStatusBar::Init(MadEditFrame* frame, wxWindowID id)
 {
 	m_frame = frame;
 	m_statusbar = new wxStatusBar(m_frame, id);
-
-#if defined(__WXGTK__)
 	m_statusbar->SetFont(wxFont(9, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL, false));
-#endif
 }
 
 void WXMStatusBar::Resize()
 {
+	// width-height ratios for fields
 #ifdef __WXMSW__
-	static int status_width_1_6 =     (220+ 305+ 135+ 155+ 65+ (50 + 10));
-	static int status_widths[7] = { 0, 220, 305, 135, 155, 65, (50 + 10) };
+	static const float W_H_RATIOS[7] = { 0.0f, 8.6f, 13.5f, 6.6f, 5.5f, 2.7f, 2.0f };
 #else
-	static int status_width_1_6 =     (220+ 305+ 135+ 155+ 65+ (50 + 0));
-	static int status_widths[7] = { 0, 220, 305, 135, 155, 65, (50 + 0) };
+	static const float W_H_RATIOS[7] = { 0.0f, 9.8f, 15.6f, 7.8f, 6.7f, 3.0f, 2.5f };
 #endif
 
-	// status bar field widths
-	int width0 = m_frame->GetClientSize().GetWidth() - status_width_1_6;
-	status_widths[0] = (width0 < 0) ? 0 : width0;
-	m_statusbar->SetFieldsCount(7, status_widths);
+	int h = m_statusbar->GetClientSize().GetHeight();
+
+#if defined(__WXMSW__) && wxMAJOR_VERSION > 2
+	static const int MARGIN = 5;
+#else
+	static const int MARGIN = 1;
+#endif
+
+	// calculate status bar field widths
+	int width_1to6 = MARGIN;
+	int widths[7] = { 0, 0, 0, 0, 0, 0, 0 };
+	for (int i = 0; i < 7; ++i)
+	{
+		int w = int(W_H_RATIOS[i] * h);
+		widths[i] = w;
+		width_1to6 += w + MARGIN*2;
+	}
+	width_1to6 += int(0.8f * h);
+
+	int width_0 = m_statusbar->GetClientSize().GetWidth() - width_1to6;
+	widths[0] = (width_0 < 0) ? 0 : width_0;
+
+	m_statusbar->SetFieldsCount(7, widths);
 }
 
 void WXMStatusBar::Attach()
