e573616422e3bf155f0b47d385cfcc89275c2383
hltj <jiaywe@gmail.com>
2023-07-02 20:47:11 +0800

    implement GB18030-2022 for ICU version lower than 73.2

--- src/xm/encoding/gb18030.cpp
+++ src/xm/encoding/gb18030.cpp
@@ -21,6 +21,18 @@
 namespace xm
 {
 
+template<typename XBYTE>
+size_t DByteIndexOf(const XBYTE* buf)
+{
+	return ((ubyte)buf[0] - 0x81) * (0xFE - 0x40 + 1) + ((ubyte)buf[1] - 0x40);
+}
+
+template<typename XBYTE>
+uint32_t MByteToUInt32(const XBYTE mbs[4])
+{
+	return ((uint32_t)(ubyte)mbs[0] << 24) | ((uint32_t)(ubyte)mbs[1] << 16) | ((uint32_t)(ubyte)mbs[2] << 8) | (ubyte)mbs[3];
+}
+
 void EncodingGB18030::MultiByteInit()
 {
 	memset(m_db2u.c_array(), svtNotCached, sizeof(ucs4_t)*BMP_DBYTE_CNT);
@@ -31,6 +43,45 @@ void EncodingGB18030::MultiByteInit()
 	memset(m_bmp2mb.c_array()+0xE000, svtNotCached, sizeof(uint32_t)*0x2000);
 
 	m_qb2u[BMP_QBYTE_IDX_FFFD] = 0x00FFFD;
+
+	// for ICU lower than 73.2
+	m_db2u[DByteIndexOf("\xA6\xD9")] = 0x00FE10;
+	m_db2u[DByteIndexOf("\xA6\xDA")] = 0x00FE12;
+	m_db2u[DByteIndexOf("\xA6\xDB")] = 0x00FE11;
+	m_db2u[DByteIndexOf("\xA6\xDC")] = 0x00FE13;
+	m_db2u[DByteIndexOf("\xA6\xDD")] = 0x00FE14;
+	m_db2u[DByteIndexOf("\xA6\xDE")] = 0x00FE15;
+	m_db2u[DByteIndexOf("\xA6\xDF")] = 0x00FE16;
+	m_db2u[DByteIndexOf("\xA6\xEC")] = 0x00FE17;
+	m_db2u[DByteIndexOf("\xA6\xED")] = 0x00FE18;
+	m_db2u[DByteIndexOf("\xA6\xF3")] = 0x00FE19;
+	m_db2u[DByteIndexOf("\xFE\x59")] = 0x009FB4;
+	m_db2u[DByteIndexOf("\xFE\x61")] = 0x009FB5;
+	m_db2u[DByteIndexOf("\xFE\x66")] = 0x009FB6;
+	m_db2u[DByteIndexOf("\xFE\x67")] = 0x009FB7;
+	m_db2u[DByteIndexOf("\xFE\x6D")] = 0x009FB8;
+	m_db2u[DByteIndexOf("\xFE\x7E")] = 0x009FB9;
+	m_db2u[DByteIndexOf("\xFE\x90")] = 0x009FBA;
+	m_db2u[DByteIndexOf("\xFE\xA0")] = 0x009FBB;
+
+	m_bmp2mb[0x009FB4] = MByteToUInt32("\xFE\x59\0");
+	m_bmp2mb[0x009FB5] = MByteToUInt32("\xFE\x61\0");
+	m_bmp2mb[0x009FB6] = MByteToUInt32("\xFE\x66\0");
+	m_bmp2mb[0x009FB7] = MByteToUInt32("\xFE\x67\0");
+	m_bmp2mb[0x009FB8] = MByteToUInt32("\xFE\x6D\0");
+	m_bmp2mb[0x009FB9] = MByteToUInt32("\xFE\x7E\0");
+	m_bmp2mb[0x009FBA] = MByteToUInt32("\xFE\x90\0");
+	m_bmp2mb[0x009FBB] = MByteToUInt32("\xFE\xA0\0");
+	m_bmp2mb[0x00FE10] = MByteToUInt32("\xA6\xD9\0");
+	m_bmp2mb[0x00FE11] = MByteToUInt32("\xA6\xDB\0");
+	m_bmp2mb[0x00FE12] = MByteToUInt32("\xA6\xDA\0");
+	m_bmp2mb[0x00FE13] = MByteToUInt32("\xA6\xDC\0");
+	m_bmp2mb[0x00FE14] = MByteToUInt32("\xA6\xDD\0");
+	m_bmp2mb[0x00FE15] = MByteToUInt32("\xA6\xDE\0");
+	m_bmp2mb[0x00FE16] = MByteToUInt32("\xA6\xDF\0");
+	m_bmp2mb[0x00FE17] = MByteToUInt32("\xA6\xEC\0");
+	m_bmp2mb[0x00FE18] = MByteToUInt32("\xA6\xED\0");
+	m_bmp2mb[0x00FE19] = MByteToUInt32("\xA6\xF3\0");
 }
 
 inline bool IsQByte2NONBMP(const ubyte* buf)
@@ -98,7 +149,7 @@ ucs4_t EncodingGB18030::QByte2BMP(const ubyte* buf)
 
 ucs4_t EncodingGB18030::DByte2BMP(const ubyte* buf)
 {
-	size_t idx = (buf[0] - 0x81)*(0xFE - 0x40 + 1) + (buf[1] - 0x40);
+	size_t idx = DByteIndexOf(buf);
 	if (m_db2u[idx] != (ucs4_t)svtUCS4NotCached)
 		return m_db2u[idx];
 
@@ -170,7 +221,7 @@ void EncodingGB18030::CacheMBofUCS4(uint32_t& mb, ucs4_t u)
 		return;
 	}
 
-	mb = (((uint32_t)mbs[0]) << 24) | (((uint32_t)mbs[1]) << 16) | (((uint32_t)mbs[2]) << 8) | mbs[3];
+	mb = MByteToUInt32(mbs);
 	m_bmp2mb[u] = mb;
 }
 
